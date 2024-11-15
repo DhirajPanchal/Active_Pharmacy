6c8a6d26790a53bed71f91418f6b508ea8175596
DhirajPanchal <dhiraj.r.panchal@gmail.com>
2024-11-15 14:56:52 +0530

    COMPLETE : BE / Generic API - SORT

--- SERVICES_BE/store-service/src/main/java/io/active/pharmacy/store/util/SqlQueryUtil.java
+++ SERVICES_BE/store-service/src/main/java/io/active/pharmacy/store/util/SqlQueryUtil.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 @Slf4j
 public class SqlQueryUtil {
@@ -14,27 +15,38 @@ public class SqlQueryUtil {
     static String activeOnly = " AND active IS TRUE";
 
     public static List<String> composeSqlQuery(String type, int index, int size, ListRequest requestBody) {
-        log.info(" _ DRUG . LIST . QUERY ");
+        log.info(" _ DRUG . LIST . SQL QUERY ");
 
         StringBuilder builder = new StringBuilder();
         builder.append(" FROM ");
         builder.append(type);
+        //   W H E R E
+        builder.append(whereClause(requestBody));
 
-        builder.append(getWhere(requestBody));
+        //   C O U N T
+        String sqlCount = "SELECT COUNT(*)" + builder.toString() + ";";
 
-        String sqlQuery = "SELECT *" + builder.toString();
-        String sqlCount = "SELECT COUNT(*)" + builder.toString();
-        System.out.println("SQL - QUERY");
+        //   O R D E R  B Y
+        builder.append(orderByClause(requestBody));
+
+        //   P A G I N A T I O N
+        builder.append(pagination(index, size));
+
+
+        //   S E L E C T
+        String sqlQuery = "SELECT *" + builder.toString() + " ;";
+
+        System.out.println("--------------------------------------------------");
         System.out.println(sqlQuery);
-        System.out.println("SQL - COUNT");
         System.out.println(sqlCount);
-
+        System.out.println("--------------------------------------------------");
         return Arrays.asList(sqlQuery, sqlCount);
 
     }
 
-    private static String getWhere(ListRequest requestBody) {
-        System.out.println("Filters***");
+
+    private static String whereClause(ListRequest requestBody) {
+        System.out.println("FILTER::");
         StringBuilder sb = new StringBuilder();
 
         sb.append(" WHERE deleted IS false");
@@ -48,20 +60,27 @@ public class SqlQueryUtil {
 
             if (requestBody.getFilter() != null) {
                 requestBody.getFilter().forEach((FilterItem filterItem) -> {
+
+
                             if (filterItem != null
                                     && !StringUtil.isNullOrEmpty(filterItem.getField())
                                     && !StringUtil.isNullOrEmpty(filterItem.getOperator())
                                     && !StringUtil.isNullOrEmpty(filterItem.getValue())
                             ) {
-                                String whereStr = " AND FIELD OPERATOR VALUE";
-                                whereStr = whereStr.replace("FIELD", filterItem.getField());
+                                System.out.println(filterItem);
 
-                                if ("contains".equals(filterItem.getOperator())) {
-                                    whereStr = whereStr.replace("OPERATOR", "LIKE");
-                                    whereStr = whereStr.replace("VALUE", ("'%" + filterItem.getValue() + "%'"));
-                                } else if ("equals".equals(filterItem.getOperator())) {
+                                String whereStr = "";
+
+                                if ("equals".equals(filterItem.getOperator())) {
+                                    whereStr = " AND FIELD OPERATOR VALUE";
+                                    whereStr = whereStr.replace("FIELD", filterItem.getField());
                                     whereStr = whereStr.replace("OPERATOR", "=");
                                     whereStr = whereStr.replace("VALUE", ("'" + filterItem.getValue() + "'"));
+                                } else if ("contains".equals(filterItem.getOperator())) {
+                                    whereStr = " AND FIELD OPERATOR VALUE";
+                                    whereStr = whereStr.replace("FIELD", filterItem.getField());
+                                    whereStr = whereStr.replace("OPERATOR", "LIKE");
+                                    whereStr = whereStr.replace("VALUE", ("'%" + filterItem.getValue() + "%'"));
                                 }
                                 sb.append(whereStr);
                             }
@@ -75,4 +94,69 @@ public class SqlQueryUtil {
         return sb.toString();
 
     }
+
+
+    private static String orderByClause(ListRequest requestBody) {
+        StringBuilder orderBy = new StringBuilder("");
+        System.out.println("SORT::");
+        if (requestBody != null && requestBody.getSort() != null) {
+            Set<String> keys = requestBody.getSort().keySet();
+            int multi = -1;
+
+            for (String key : keys) {
+                multi++;
+
+                System.out.println(multi + " - " + key + " [ " + requestBody.getSort().get(key) + " ]");
+                String order = requestBody.getSort().get(key);
+                if (multi == 0) {
+                    orderBy.append(" ORDER BY ");
+                }
+                if (multi > 0) {
+                    orderBy.append(", ");
+                }
+                orderBy.append(key + " " + order);
+
+
+            }
+
+        }
+        System.out.println(orderBy.toString());
+
+        return orderBy.toString();
+    }
+
+
+    private static String pagination(int index, int size) {
+        String page = " LIMIT " + size;
+        if (index == 0) {
+            page += " OFFSET 0";
+        } else if (index > 0) {
+            page += " OFFSET " + (size * index);
+        }
+
+        return page;
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
